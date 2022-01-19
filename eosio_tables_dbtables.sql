d4616a4ea73818e3b26dec856fd64a93705071bc
cc32d9 <cc32d9@gmail.com>
2022-01-19 21:26:59 +0000

    split dbsetup script

---
+++ eosio_tables_dbtables.sql
@@ -0,0 +1,36 @@
+CREATE TABLE SYNC
+(
+ network           VARCHAR(15) PRIMARY KEY,
+ block_num         BIGINT NOT NULL,
+ block_time        DATETIME NOT NULL,
+ irreversible      BIGINT NOT NULL
+) ENGINE=InnoDB;
+
+
+CREATE TABLE CONTRACT_LAST_UPD
+(
+ network           VARCHAR(15) NOT NULL,
+ contract          VARCHAR(13) NOT NULL,
+ block_num         BIGINT NOT NULL
+) ENGINE=InnoDB;
+
+CREATE UNIQUE INDEX CONTRACT_LAST_UPD_I01 ON CONTRACT_LAST_UPD(network, contract);
+
+
+
+CREATE TABLE WATCH_CONTRACTS
+(
+ network           VARCHAR(15) NOT NULL,
+ contract          VARCHAR(13) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE UNIQUE INDEX WATCH_CONTRACTS_I01 ON WATCH_CONTRACTS(network, contract);
+
+
+CREATE TABLE EXPORT_TBL_UPDATES
+(
+ network           VARCHAR(15) NOT NULL,
+ contract          VARCHAR(13) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE UNIQUE INDEX EXPORT_TBL_UPDATES_I01 ON EXPORT_TBL_UPDATES(network, contract);
